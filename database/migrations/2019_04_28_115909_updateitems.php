866b07fbd7b0947f8480f59206af08a02dd7dc76
Leo <hello@lei.ph>
2019-05-19 14:03:09 +0400

    Require doctrine/dbal, fix category_id migration.

--- database/migrations/2019_04_28_115909_updateitems.php
+++ database/migrations/2019_04_28_115909_updateitems.php
@@ -14,7 +14,7 @@ class Updateitems extends Migration
     public function up()
     {
         Schema::table('items', function (Blueprint $table) {
-            $table->unsignedinteger('catagory_id');
+            $table->unsignedinteger('catagory_id')->change();
         });
     }
 
