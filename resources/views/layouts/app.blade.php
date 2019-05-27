cd1eda10a37843225e98b310e8c1529890709dcf
Rizvi <r.iqbal@almani.ae>
2019-05-27 14:32:17 +0400

    fb login fix try

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,9 +19,34 @@
     <link rel="stylesheet" type="text/css" href="/css/dashboard.css">
 </head>
 <body>
+    <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                appId      : '2035159810123718',
+                cookie     : true,
+                xfbml      : true,
+                version    : 'v3.3'
+            });
+      
+            FB.AppEvents.logPageView();   
+            FB.getLoginStatus(function(response) {
+                statusChangeCallback(response);
+            });
+      
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+    </script>
     @include('include.navbar')
     @yield('content')
-    <footer class="text-white bg-dark footer-content ">
+  <footer class="text-white bg-dark footer-content ">
             <div class="container">
                 <div class="row">
                     <div class="col-md-4 footer-container">
