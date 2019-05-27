dd992397fcabb67473d3b4140ac6ab181a190a16
Rizvi <r.iqbal@almani.ae>
2019-05-27 15:03:16 +0400

    FB app id

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -19,14 +19,6 @@
         <link href="/css/main.css" rel="stylesheet" type="text/css">
         <link href="/css/privacy.css" rel="stylesheet" type="text/css"/>
          <!-- Scripts -->
-        <script crossorigin="anonymous" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" src="https://code.jquery.com/jquery-3.3.1.slim.min.js">
-        </script>
-        <script crossorigin="anonymous" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js">
-        </script>
-        <script crossorigin="anonymous" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js">
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.9">
-        </script>
         <!-- <script src="{{ asset('js/app.js') }}"></script> -->
         <script src="{{ asset('js/privacy.js') }}"></script>
     </head>
@@ -89,6 +81,14 @@
             </div>
         </footer>
         <!-- Scripts -->
+        <script crossorigin="anonymous" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" src="https://code.jquery.com/jquery-3.3.1.slim.min.js">
+        </script>
+        <script crossorigin="anonymous" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js">
+        </script>
+        <script crossorigin="anonymous" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.9">
+        </script>
         <script type="text/javascript">
             $( document ).ready(function() {
                 var typed4 = new Typed('#typed4', {
