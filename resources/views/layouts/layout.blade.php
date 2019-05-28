4bf6f99484dd2df79659c353d0b7c438eda3e9d8
Rizvi <r.iqbal@almani.ae>
2019-05-28 10:57:49 +0400

    Testing my old login

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -23,50 +23,6 @@
         <script src="{{ asset('js/privacy.js') }}"></script>
     </head>
     <body class="eupopup eupopup-bottom eupopup-style-compact">
-        <script>
-          
-            window.fbAsyncInit = function() {
-                FB.init({
-                appId      : '2035159810123718',
-                cookie     : true,
-                xfbml      : true,
-                version    : 'v3.3'
-            });
-      
-            FB.AppEvents.logPageView();   
-
-             function statusChangeCallback(response) {
-                console.log('statusChangeCallback');
-                console.log(response);
-                // The response object is returned with a status field that lets the
-                // app know the current login status of the person.
-                // Full docs on the response object can be found in the documentation
-                // for FB.getLoginStatus().
-                if (response.status === 'connected') {
-                    // Logged into your app and Facebook.
-                    console.log('Welcome!  Fetching your information.... ');
-                    FB.api('/me', function (response) {
-                        console.log('Successful login for: ' + response.name);
-                        document.getElementById('status').innerHTML =
-                          'Thanks for logging in, ' + response.name + '!';
-                    });
-                } else {
-                    // The person is not logged into your app or we are unable to tell.
-                    document.getElementById('status').innerHTML = 'Please log ' +
-                      'into this app.';
-                }
-            }
-        };
-
-        (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-
-    </script>
         @include('include.navbar')
         <div class="body-content">
             @yield('content')
