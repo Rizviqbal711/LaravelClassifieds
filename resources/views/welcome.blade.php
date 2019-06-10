db767e5656b6fdce3859e74b00f93e4e59968134
Rizvi <r.iqbal@almani.ae>
2019-06-10 14:55:11 +0400

    video update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -147,7 +147,7 @@
     </div>
     <div class=" container-fluid row justify-content-center align-items-center ">
         <div class="embed-responsive embed-responsive-16by9 video">
-             <iframe width="640" height="360" src="https://www.youtube.com/embed/FTnvZotAuuc?rel=0&enablejsapi=1" frameborder="0" allowfullscreen></iframe>
+            <iframe width="640" height="360" src="https://www.youtube.com/embed/ty6bRi9w4S4?rel=0&enablejsapi=1" frameborder="0" allowfullscreen></iframe>
         </div>  
     </div>
     <div class="row justify-content-center align-items-center mt-5">
