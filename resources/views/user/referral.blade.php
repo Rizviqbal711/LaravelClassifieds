9f6046a9f42cd544c67154b30d6a5a5935a8435e
Rizvi <r.iqbal@almani.ae>
2019-06-25 08:35:01 +0400

    referral copy done

--- resources/views/user/referral.blade.php
+++ resources/views/user/referral.blade.php
@@ -1,30 +1,32 @@
 @extends(($agent->isMobile()) ? 'layouts.mobile-layout' : 'layouts.layout')
 
+
+@section('title', 'Referral | QuickList')
+
 @section('content')
 <div class="container myitem-content">
 	<h2>Referrals</h2>
 	<p>Refer a friend with this link and earn 20 points</p>
 	<div class="card">
 		<div class="card-body">
-			<p>You Referral Link</p>
-			<div>
-				<span class="border border-success p-2 copy-to-clipboard">
-					<input type="hidden" value="{{ auth()->user()->getReferrals()[0]->link }}" disabled="disabled" >
-					{{ auth()->user()->getReferrals()[0]->code }}
-				</span>
-				<button type="submit" class="btn btn-success btn-md" onclick="myFunction('#referral-code');">Copy</button>
+			<p>Your Referral Link</p>
+			<input type="text" value="{{ auth()->user()->getReferrals()[0]->link }}" class="allowCopy form-control" readonly>
+			<small>Please click the link to copy</small>
+			<div class="alert alert-success text-center alert fade-in mt-2">
+				Link Copied!
 			</div>
 		</div>
 	</div>
 </div>
 <script type="text/javascript">
-$(function() {
-	$('.copy-to-clipboard input').click(function() {
-	$(this).focus();
-	$(this).select();
-	document.execCommand('copy');
-	$(".copied").text("Copied to clipboard").show().fadeOut(1200);
-    });
+  $(document).ready(function(){
+   $('.allowCopy').click(function() {
+   	// $(this).css('background', 'yellow');
+     $(this).focus();
+     $(this).select();
+     document.execCommand('copy');
+     $('.alert').show();
+   });
 });
 </script>
 
